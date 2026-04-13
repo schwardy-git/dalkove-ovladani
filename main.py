3942669a9af4abb1d5028f06f7306ea4a456f13d
schwardy-git <tomas.schwardy@gmail.com>
2026-04-13 18:43:47 +0200

    Aktualizovat pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 def on_ir_callback(serv1):
-    console.log(serv1)
-    pass
+    print("serv")
+    basic.show_number(serv1)
+    print(serv1)
+    
 cuteBot.IR_callback(on_ir_callback)
 
 led.unplot(2, 3)
 
 def on_forever():
-    print("ab")
     if cuteBot.IR_Button(cuteBot.IRButtons.TWO):
         led.plot(2, 3)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
-cuteBot.IR_callback(function (serv1) {
+cuteBot.IR_callback(function on_ir_callback(serv1: number) {
+    console.log("serv")
+    basic.showNumber(serv1)
     console.log(serv1)
 })
 led.unplot(2, 3)
-basic.forever(function () {
-    console.log("ab")
-if (cuteBot.IR_Button(cuteBot.IRButtons.Two)) {
+basic.forever(function on_forever() {
+    if (cuteBot.IR_Button(cuteBot.IRButtons.Two)) {
         led.plot(2, 3)
     }
+    
 })
